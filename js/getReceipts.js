fa38a4cfd1ee0721a5dc5e50a04834cb0e62f909
tdharris <tylerdavidharris@gmail.com>
2014-03-04 00:14:47 -0700

    cleaned up css/js, added loading-overlay css, started worker threads

---
+++ js/getReceipts.js
@@ -0,0 +1,77 @@
+/* This function gets the receipts and populates the view */ 
+function getReceipts(){
+	
+	/* Get the JSON Object from Local Stoarage */
+	var library = JSON.parse(localStorage.getItem("pbReceipts"));
+
+	/* Get the Reciept Library */
+	var view = document.getElementById("library");
+
+	/* This loop creates the month and Receipts containers */
+	for(var i = 0; i < library.containers.length; i++){
+
+		/* Create the Month Container */
+		var monthli = document.createElement("li");
+
+		/* Create the Container Month Name */
+		var monthH2 = document.createElement("h2");
+
+		/* Create the Receipt Container */ 
+		var receiptsUL= document.createElement("ul");
+		var monthText = library.containers[i].month + " " + library.containers[i].year;
+
+
+		/* Append the elements */ 
+		monthH2.innerText = monthText; 
+		monthli.appendChild(monthH2);
+		
+		monthli.appendChild(receiptsUL);
+
+		view.appendChild(monthli);
+
+
+	 	/* This loops Creates the Receipts Objects */
+		for(var j = 0; j < library.containers[i].receipts.length; j++){
+
+			/* Create the Receipt Object */ 
+			var receiptLI = document.createElement("li");
+
+			/* Create the Image for the Receipt */ 
+			var image = document.createElement("img");
+			image.style.height = "180px";
+			image.style.width = "90px"
+
+			var url = library.containers[i].receipts[j].imgURL;
+			image.src = url;
+
+			/* Create the Tags Container */ 
+			var tags = document.createElement("ul");
+
+			/* Append the iamge to the Reciept Object */ 
+			receiptsUL.appendChild(receiptLI);
+			receiptLI.appendChild(image);
+			receiptLI.appendChild(tags);
+
+			tags.className = "tag-overlay";
+
+			/* This loop creates the tags objects */
+			for(var k = 0; k < library.containers[i].receipts[j].tags.length; k++){
+
+				/* Create the Tag */ 
+				var tagLI = document.createElement("li");
+
+				var tagText = library.containers[i].receipts[j].tags[k];
+
+				tagLI.innerHTML = tagText;
+
+				/* Append the Tag to the Containter */ 
+				tags.appendChild(tagLI);
+
+			}
+		}
+	}
+}
+
+self.addEventListener('message', function(e) {
+  getReceipts();
+}, false);
---
+++ js/utility.js
@@ -0,0 +1,42 @@
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+
+function addClass(element, classToAdd) {
+	element = document.getElementById(element);
+    var currentClassValue = element.className;
+      
+    if (currentClassValue.indexOf(classToAdd) == -1) {
+        if ((currentClassValue == null) || (currentClassValue === "")) {
+            element.className = classToAdd;
+        } else {
+            element.className += " " + classToAdd;
+        }
+    }
+}
+ 
+function removeClass(element, classToRemove) {
+	element = document.getElementById(element);
+    var currentClassValue = element.className;
+ 
+    if (currentClassValue == classToRemove) {
+        element.className = "";
+        return;
+    }
+ 
+    var classValues = currentClassValue.split(" ");
+    var filteredList = [];
+ 
+    for (var i = 0 ; i < classValues.length; i++) {
+        if (classToRemove != classValues[i]) {
+            filteredList.push(classValues[i]);
+        }
+    }
+ 
+    element.className = filteredList.join(" ");
+}
